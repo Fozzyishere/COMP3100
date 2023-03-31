e0d4cbbd993df6256f5c596e386734039a288d10
Fozzyishere <danielpham13092002@gmail.com>
2023-03-31 13:17:47 +1100

    add comments

--- client.java
+++ client.java
@@ -1,32 +1,40 @@
-
 import java.net.*;
 import java.io.*;
 
 public class client {
     public static void main(String args[]) throws Exception {
+        //var declaration
         Socket s = new Socket("localhost", 50000);
         BufferedReader din = new BufferedReader(new InputStreamReader(s.getInputStream()));
         DataOutputStream dout = new DataOutputStream(s.getOutputStream());
         String DATAString = "";
         int[] DATAValues = new int[2];
+        int nRecs = 0;
+
+        //print server connect
+        System.out.println("java server started, connection is on!");
 
         // send hello
         dout.write(("HELO\n").getBytes());
         System.out.println(din.readLine());
 
-        // send auth
+        // send auth (handshake)
         String username = System.getProperty("user.name");
         dout.write(("AUTH " + username + "\n").getBytes());
         System.out.println(din.readLine());
 
         // loop
-        while (!din.readLine().equals("")) {
+        while (!din.readLine().equals("NONE")) {
             dout.write(("REDY\n").getBytes());
             System.out.println(din.readLine());
             dout.write(("GETS All\n").getBytes());
             DATAString = din.readLine();
             System.out.println(din.readLine());
-
+            DATAValues = convertStringToInt(storeNumber(extractNumber(DATAString)));
+            nRecs = DATAValues[0];
+            for(int i =0; i < nRecs; i++){
+                System.out.println(din.readLine());
+            }
         }
 
         dout.close();
